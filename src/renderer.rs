475e1fa70cb673c0fa9198de432e72e4812a26f5
pingy <arpanbajpai8@gmail.com>
2025-08-12 14:10:15 +0530

    Added buffered triangles

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
-use wgpu::hal::noop::Encoder;
+use wgpu::{hal::noop::Encoder, util::DeviceExt};
 use winit::window::Window;
+
+use super::mesh;
 
 #[allow(unused)]
 pub struct GpuContext {
@@ -11,6 +13,8 @@ pub struct GpuContext {
     sufrace_config: wgpu::SurfaceConfiguration,
     is_surface_configured: bool,
     adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
 }
 
 impl GpuContext {
@@ -56,6 +60,22 @@ impl GpuContext {
             desired_maximum_frame_latency: 2,
         };
 
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                // WebGL doesn't support all of wgpu's features, so if
+                // we're building for the web we'll have to disable some.
+                required_limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            })
+            .await?;
+
         return Ok(GpuContext {
             instance: instance,
             adapter: adapter,
@@ -63,15 +83,17 @@ impl GpuContext {
             sufrace_config: config,
             is_surface_configured: false,
             window: window,
+            device: device,
+            queue: queue,
         });
     }
 
-    pub fn resize(&mut self, width: u32, height: u32, render_context: &RenderContext) {
+    pub fn resize(&mut self, width: u32, height: u32) {
         if width > 0 && height > 0 {
+            println!("Resizing");
             self.sufrace_config.width = width;
             self.sufrace_config.height = height;
-            self.surface
-                .configure(&render_context.device, &self.sufrace_config);
+            self.surface.configure(&self.device, &self.sufrace_config);
             self.is_surface_configured = true;
         }
     }
@@ -90,96 +112,6 @@ impl GpuContext {
             .get_current_texture()
             .expect("Failed to get surface texture");
     }
-}
-
-pub struct RenderContext {
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    pipeline: wgpu::RenderPipeline,
-}
-
-impl RenderContext {
-    pub async fn new(gpu_context: &GpuContext) -> anyhow::Result<RenderContext> {
-        let (device, queue) = gpu_context
-            .adapter
-            .request_device(&wgpu::DeviceDescriptor {
-                label: None,
-                required_features: wgpu::Features::empty(),
-                // WebGL doesn't support all of wgpu's features, so if
-                // we're building for the web we'll have to disable some.
-                required_limits: if cfg!(target_arch = "wasm32") {
-                    wgpu::Limits::downlevel_webgl2_defaults()
-                } else {
-                    wgpu::Limits::default()
-                },
-                memory_hints: Default::default(),
-                trace: wgpu::Trace::Off,
-            })
-            .await?;
-
-        let shader = device.create_shader_module(wgpu::include_wgsl!("shaders/shader.wgsl"));
-
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-                label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
-                push_constant_ranges: &[],
-            });
-
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: Some("vs_main"),
-                buffers: &[],
-                compilation_options: Default::default(),
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: Some("fs_main"),
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: gpu_context.sufrace_config.format,
-                    blend: Some(wgpu::BlendState {
-                        color: wgpu::BlendComponent::REPLACE,
-                        alpha: wgpu::BlendComponent::REPLACE,
-                    }),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-                compilation_options: Default::default(),
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
-                // or Features::POLYGON_MODE_POINT
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: None,
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            // If the pipeline will be used with a multiview render pass, this
-            // indicates how many array layers the attachments will have.
-            multiview: None,
-            // Useful for optimizing shader compilation on Android
-            cache: None,
-        });
-
-        return Ok(RenderContext {
-            device: device,
-            queue: queue,
-            pipeline: render_pipeline,
-        });
-    }
 
     fn create_encoder(&self) -> wgpu::CommandEncoder {
         return self
@@ -187,6 +119,93 @@ impl RenderContext {
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
                 label: Some("Main encoder"),
             });
+    }
+}
+
+pub struct RenderContext {
+    pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+}
+
+impl RenderContext {
+    pub async fn new(gpu_context: &GpuContext) -> anyhow::Result<RenderContext> {
+        let shader = gpu_context
+            .device
+            .create_shader_module(wgpu::include_wgsl!("shaders/shader.wgsl"));
+
+        let render_pipeline_layout =
+            gpu_context
+                .device
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[],
+                    push_constant_ranges: &[],
+                });
+
+        let render_pipeline =
+            gpu_context
+                .device
+                .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                    label: Some("Render Pipeline"),
+                    layout: Some(&render_pipeline_layout),
+                    vertex: wgpu::VertexState {
+                        module: &shader,
+                        entry_point: Some("vs_main"),
+                        buffers: &[mesh::Vertex::get_vertex_descriptor()],
+                        compilation_options: Default::default(),
+                    },
+                    fragment: Some(wgpu::FragmentState {
+                        module: &shader,
+                        entry_point: Some("fs_main"),
+                        targets: &[Some(wgpu::ColorTargetState {
+                            format: gpu_context.sufrace_config.format,
+                            blend: Some(wgpu::BlendState {
+                                color: wgpu::BlendComponent::REPLACE,
+                                alpha: wgpu::BlendComponent::REPLACE,
+                            }),
+                            write_mask: wgpu::ColorWrites::ALL,
+                        })],
+                        compilation_options: Default::default(),
+                    }),
+                    primitive: wgpu::PrimitiveState {
+                        topology: wgpu::PrimitiveTopology::TriangleList,
+                        strip_index_format: None,
+                        front_face: wgpu::FrontFace::Ccw,
+                        cull_mode: Some(wgpu::Face::Back),
+                        // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
+                        // or Features::POLYGON_MODE_POINT
+                        polygon_mode: wgpu::PolygonMode::Fill,
+                        // Requires Features::DEPTH_CLIP_CONTROL
+                        unclipped_depth: false,
+                        // Requires Features::CONSERVATIVE_RASTERIZATION
+                        conservative: false,
+                    },
+                    depth_stencil: None,
+                    multisample: wgpu::MultisampleState {
+                        count: 1,
+                        mask: !0,
+                        alpha_to_coverage_enabled: false,
+                    },
+                    // If the pipeline will be used with a multiview render pass, this
+                    // indicates how many array layers the attachments will have.
+                    multiview: None,
+                    // Useful for optimizing shader compilation on Android
+                    cache: None,
+                });
+
+        let vertex_buffer =
+            gpu_context
+                .device
+                .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                    label: Some("Vertex Buffer"),
+                    contents: bytemuck::cast_slice(mesh::VERTICES),
+                    usage: wgpu::BufferUsages::VERTEX,
+                });
+
+        return Ok(RenderContext {
+            pipeline: render_pipeline,
+            vertex_buffer: vertex_buffer,
+        });
     }
 }
 
@@ -208,7 +227,7 @@ impl Renderer {
     }
 
     pub fn resize(&mut self, width: u32, height: u32) {
-        self.gpu_context.resize(width, height, &self.render_context);
+        self.gpu_context.resize(width, height);
     }
 
     pub fn render(&mut self) -> anyhow::Result<()> {
@@ -224,7 +243,7 @@ impl Renderer {
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
 
-        let mut encoder = self.render_context.create_encoder();
+        let mut encoder = self.gpu_context.create_encoder();
 
         {
             let mut _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
@@ -249,10 +268,11 @@ impl Renderer {
             });
 
             _render_pass.set_pipeline(&self.render_context.pipeline);
-            _render_pass.draw(0..3, 0..1);
+            _render_pass.set_vertex_buffer(0, self.render_context.vertex_buffer.slice(..));
+            _render_pass.draw(0..(mesh::VERTICES.len() as u32), 0..1);
         }
 
-        self.render_context
+        self.gpu_context
             .queue
             .submit(std::iter::once(encoder.finish()));
         output.present();
